767a0ce7ac5cbbf87bad722a52b9968b3c8045ec
Matt Tennison <matt@matttennison.com>
2021-03-13 22:05:42 +0000

    Linting fix

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -1,15 +1,20 @@
 const { resolve } = require("path");
 
 module.exports = {
-    plugins: [{
-        name: 'bundle-analyzer',
-        options: {
-            bundleAnalyzerConfig: {
-                openAnalyzer: false,
-                generateStatsFile: true,
-                statsFilename: resolve(__dirname, './build/metadata/stats.json'),
-                reportFilename: resolve(__dirname, './build/metadata/bundle-size-report.html')
-            }
-        }
-    }]
-}
+  plugins: [
+    {
+      name: "bundle-analyzer",
+      options: {
+        bundleAnalyzerConfig: {
+          openAnalyzer: false,
+          generateStatsFile: true,
+          statsFilename: resolve(__dirname, "./build/metadata/stats.json"),
+          reportFilename: resolve(
+            __dirname,
+            "./build/metadata/bundle-size-report.html"
+          ),
+        },
+      },
+    },
+  ],
+};
