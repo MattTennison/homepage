58affcbe8e1af481842e24980915d3419c9b4039
Matt Tennison <matt@matttennison.com>
2021-03-13 17:36:53 +0000

    Add Chakra UI and "Hello World" page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Box, ChakraProvider, Container } from '@chakra-ui/react';
 
 export const App = () => (
-  <p>Hello World</p>
+  <ChakraProvider>
+    <Box backgroundImage="url(https://images.pexels.com/photos/2527556/pexels-photo-2527556.jpeg)" w="100vw" h="100vh" backgroundSize="cover" backgroundPosition="center" display="flex" alignItems="center" justifyContent="center">
+      <Container backgroundColor="white" padding={4} borderWidth={2} borderColor="whiteAlpha.600" backgroundClip="padding-box">
+        Hello World
+      </Container>
+    </Box>
+  </ChakraProvider>
 );
