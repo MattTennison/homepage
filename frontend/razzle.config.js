11344a0beb917452914b1acd453d8a3bc06996d4
Matt Tennison <matt@matttennison.com>
2021-03-13 22:20:36 +0000

    Disable webpack bundlesize warning
    This warning becomes an error in CI, and I haven't thought a lot about
    what performance metrics matter, or how to effectively bundle split yet

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -17,4 +17,19 @@ module.exports = {
       },
     },
   ],
+  modifyWebpackConfig({
+    env: {
+      target, // the target 'node' or 'web'
+      dev, // is this a development build? true or false
+    },
+    webpackConfig, // the created webpack config
+  }) {
+    if (dev === false && target === 'web') {
+        //note: plan to re-enable once the app is more fleshed out, and can think about lazy loading
+        webpackConfig.performance = {
+            hints: false
+        }
+    }
+    return webpackConfig;
+  },
 };
