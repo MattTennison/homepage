4c25e983b3baecda60256e230afe34c7af1196f6
Matt Tennison <matt@matttennison.com>
2021-03-13 22:03:20 +0000

    Build frontend in GH Actions
    With bundle reports in artifacts

--- frontend/razzle.config.js
+++ frontend/razzle.config.js
@@ -1,3 +1,15 @@
+const { resolve } = require("path");
+
 module.exports = {
-    plugins: ['bundle-analyzer']
+    plugins: [{
+        name: 'bundle-analyzer',
+        options: {
+            bundleAnalyzerConfig: {
+                openAnalyzer: false,
+                generateStatsFile: true,
+                statsFilename: resolve(__dirname, './build/metadata/stats.json'),
+                reportFilename: resolve(__dirname, './build/metadata/bundle-size-report.html')
+            }
+        }
+    }]
 }
